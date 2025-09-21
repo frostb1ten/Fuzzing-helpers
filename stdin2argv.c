73cc1864d93566c73dd12b8e1cc445ae69e3faca
Frostb1te <68353531+frostb1ten@users.noreply.github.com>
2025-09-21 16:16:20 -0500

    Update stdin2argv.c
    Removing limitation on specific args

--- stdin2argv.c
+++ stdin2argv.c
@@ -15,9 +15,7 @@ typedef int (*libc_start_main_fn)(
 
 static libc_start_main_fn real_start = NULL;
 static int (*real_main_fn)(int, char **, char **) = NULL;
-#define ALLOWED_FLAGS_ENV "ALLOWED_FLAGS"
 #define MAX_INPUT_SIZE (1 << 12)  /* 4KB buffer */
-#define MAX_ARGV_SIZE 512         /* Max arguments */
 
 static void log_error(const char *format, ...) {
     va_list args;
@@ -26,44 +24,9 @@ static void log_error(const char *format, ...) {
     va_end(args);
 }
 
-static int allow_flag(const char *flag) {
-    if (!flag || flag[0] != '-') {
-        return 0;
-    }
-    if (getenv("ALLOW_ALL_FLAGS")) {
-        return 1;
-    }
-    static const char *default_allowed[] = {"-V", "-h", "-K", "-C", "-T", "-n", NULL};
-    const char *env_flags = getenv(ALLOWED_FLAGS_ENV);
-    char *flags_copy = NULL;
-    int allowed = 0;
-    for (int i = 0; default_allowed[i]; i++) {
-        if (strcmp(flag, default_allowed[i]) == 0) {
-            return 1;
-        }
-    }
-    if (env_flags) {
-        flags_copy = strdup(env_flags);
-        if (!flags_copy) {
-            log_error("stdin2argv: Failed to allocate memory for flags\n");
-            return 0;
-        }
-        char *token = strtok(flags_copy, ",");
-        while (token) {
-            if (strcmp(flag, token) == 0) {
-                allowed = 1;
-                break;
-            }
-            token = strtok(NULL, ",");
-        }
-        free(flags_copy);
-    }
-    return allowed;
-}
-
 static int wrapped_main(int argc, char **argv, char **envp) {
     static char buf[MAX_INPUT_SIZE];
-    static char *new_argv[MAX_ARGV_SIZE];
+    char **new_argv = NULL;
     ssize_t n;
     n = read(STDIN_FILENO, buf, MAX_INPUT_SIZE - 1);
     if (n < 0) {
@@ -75,23 +38,49 @@ static int wrapped_main(int argc, char **argv, char **envp) {
         return real_main_fn(argc, argv, envp);
     }
     buf[n] = '\0';
+    
     int new_argc = 0;
-    char *token = strtok(buf, " \t\r\n");
+    char *temp_buf = strdup(buf);
+    if (!temp_buf) {
+        log_error("stdin2argv: Failed to allocate memory for token counting\n");
+        return real_main_fn(argc, argv, envp);
+    }
+    char *token = strtok(temp_buf, " \t\r\n");
     if (token && token[0] != '\0') {
-        new_argv[new_argc++] = token;
+        new_argc++;
+        while (strtok(NULL, " \t\r\n")) {
+            new_argc++;
+        }
+    } else {
+        new_argc = 1;
+    }
+    free(temp_buf);
+
+    new_argv = malloc((new_argc + 1) * sizeof(char *));
+    if (!new_argv) {
+        log_error("stdin2argv: Failed to allocate memory for new_argv\n");
+        return real_main_fn(argc, argv, envp);
+    }
+    int i = 0;
+    token = strtok(buf, " \t\r\n");
+    if (token && token[0] != '\0') {
+        new_argv[i++] = token;
         token = strtok(NULL, " \t\r\n");
     } else {
-        new_argv[new_argc++] = argv[0];
+        new_argv[i++] = argv[0];
     }
-    while (token && new_argc < MAX_ARGV_SIZE - 1) {
-        new_argv[new_argc++] = allow_flag(token) ? token : (char *)"-V";
+    while (token && i < new_argc) {
+        new_argv[i++] = token;
         token = strtok(NULL, " \t\r\n");
     }
-    new_argv[new_argc] = NULL;
+    new_argv[i] = NULL;
     extern char *program_invocation_name, *program_invocation_short_name;
     program_invocation_name = new_argv[0];
     program_invocation_short_name = new_argv[0];
-    return real_main_fn(new_argc, new_argv, envp);
+
+    int ret = real_main_fn(new_argc, new_argv, envp);
+    free(new_argv);
+    return ret;
 }
 
 int __libc_start_main(
